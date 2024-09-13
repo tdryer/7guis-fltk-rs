050e98761db5a88bb7ac11cd8e9df8fd3dc452e3
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-09-13 17:41:14 +0300

    add cascade macros

--- src/counter.rs
+++ src/counter.rs
@@ -1,34 +1,59 @@
-use fltk::{app::*, button::*, output::*, prelude::*, window::*};
+use {
+    cascade::cascade,
+    fltk::{app::*, button::Button, enums::Event, output::Output, prelude::*, window::Window},
+    std::{cell::RefCell, rc::Rc},
+};
 
 const WIDGET_HEIGHT: i32 = 25;
 const WIDGET_PADDING: i32 = 10;
 const WIDGET_WIDTH: i32 = 70;
+const UPDATE: Event = Event::from_i32(404);
 
 fn main() {
     let app = App::default().with_scheme(Scheme::Gtk);
-    let mut wind = Window::default()
-        .with_size(
+    let state = Rc::new(RefCell::new(0u8));
+    cascade!(
+        Window::default().with_size(
             WIDGET_WIDTH * 2 + 3 * WIDGET_PADDING,
             WIDGET_HEIGHT + WIDGET_PADDING * 2,
-        )
-        .with_label("Counter");
-
-    let mut output = Output::default()
-        .with_size(WIDGET_WIDTH, WIDGET_HEIGHT)
-        .with_pos(WIDGET_PADDING, WIDGET_PADDING);
-    output.set_value("0");
-
-    let mut button = Button::default()
-        .with_size(WIDGET_WIDTH, WIDGET_HEIGHT)
-        .right_of(&output, WIDGET_PADDING)
-        .with_label("Count");
-    let mut value = 0;
-    button.set_callback(move |_| {
-        value += 1;
-        output.set_value(&format!("{}", value));
-    });
-
-    wind.end();
-    wind.show();
+        );
+        ..set_label("Counter");
+        ..set_callback(move |_| {
+            if event() == Event::Close {
+                quit();
+            }
+        });
+        ..add(&cascade!(
+            Output::default();
+            ..set_size(WIDGET_WIDTH, WIDGET_HEIGHT);
+            ..set_pos(WIDGET_PADDING, WIDGET_PADDING);
+            ..handle({
+                let state = state.clone();
+                move |output, event| {
+                    if event == UPDATE {
+                        output.set_value(&state.borrow().to_string());
+                        return true;
+                    };
+                    false
+                }
+            });
+            ..handle_event(UPDATE);
+        ));
+        ..add(&cascade!(
+            Button::default();
+            ..set_size(WIDGET_WIDTH, WIDGET_HEIGHT);
+            ..set_pos(WIDGET_PADDING * 2 + WIDGET_WIDTH, WIDGET_PADDING);
+            ..set_label("Count");
+            ..set_callback({
+                let state = state.clone();
+                move |_| {
+                    *state.borrow_mut() += 1;
+                    handle_main(UPDATE).unwrap();
+                }
+            });
+        ));
+        ..end();
+    )
+    .show();
     app.run().unwrap();
 }
